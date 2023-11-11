ce45d49b60fb47c8ad443e5a14b67b5191501ba8
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-11 15:27:19 -0300

    Initial Javascript application - Capturing values.

---
+++ Convert Money/main.js
@@ -0,0 +1,9 @@
+
+const btn = document.querySelector(".btn_convert");
+
+btn.addEventListener("click", function() {
+    let value = document.querySelector(".value");
+    let valor_digitado = value.value;
+    console.log("Valor digitado pelo usuário: " + valor_digitado);
+});
+
