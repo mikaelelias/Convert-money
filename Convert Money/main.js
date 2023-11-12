2c81d7252a4fb451b87babc183a71fcbe860203a
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-12 11:05:10 -0300

    JS code and extension improvements

--- Convert Money/main.js
+++ Convert Money/main.js
@@ -9,6 +9,9 @@ function convertValues() {
 
     const dolarToday = 4.91
     const euroToday = 5.26
+    const libraToday = 6.00
+    const bitcoin = 182.008
+
 
     if (currencySelect.value == "dolar") {
         CurrencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -23,6 +26,21 @@ function convertValues() {
             currency: "EUR"
         }).format(input_CurrencyValue / euroToday)
     }
+
+    if (currencySelect.value == "libra") {
+        CurrencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(input_CurrencyValue / libraToday)
+    }
+    
+    if (currencySelect.value == "bitcoin") {
+        CurrencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }).format(input_CurrencyValue / bitcoin)
+    }
+    
 
     CurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -42,6 +60,16 @@ function changeCurrent() {
     if (currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro"
         currencyImage.src = "./assets/euro.png"
+    }
+
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/libra.png"
+    }
+    
+    if (currencySelect.value == "bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImage.src = "./assets/bitcoin.png"
     }
 
     convertValues()
