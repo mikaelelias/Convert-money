1498d5a26875f1edffa617d1de284ecd497dbc1a
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-11-16 22:32:22 -0300

    Delete Exercicios/Modulo_1

--- Exercicios/Modulo_1/ola_mundo/main.js
+++
@@ -1,9 +0,0 @@
-const myButton = document.querySelector('.btn-welcome');
-
-myButton.addEventListener('click', function() {
-  const newTextContainer = document.querySelector('.message_to_user');
-  console.log('Welcome, User')
-
-  newTextContainer.innerHTML = 'Welcome, user!'; 
-  newTextContainer.appendChild(newText);
-});
